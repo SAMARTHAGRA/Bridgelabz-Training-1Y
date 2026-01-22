164003184d8116ec0b4ba247584035eaa49fde29
SAMARTHAGRA <samarthagrawal766@gmail.com>
2026-01-22 15:29:05 +0530

    first commit

---
+++ Basic Programmes/Increment.java
@@ -0,0 +1,20 @@
+public class increment{
+public static  void main(string[]args){
+int i=0;
+system.out.println(i++);
+i++;
+system.out.println(i++) 
+system.out.println(i);
+system.out.println(i++);
+
+for(int k=0;k<5;k++){
+system.out.println("for loop"+k);}
+int j=10;
+while (j>10){
+system.out.println("while loop"+j);
+j--;
+}
+}
+}
+
+   
