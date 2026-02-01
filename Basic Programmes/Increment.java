c0f5043b41330435278f2ea487b3cf3a19a8f8bc
SAMARTHAGRA <samarthagrawal766@gmail.com>
2026-02-01 11:10:16 +0530

    homework

--- Basic Programmes/Increment.java
+++ Basic Programmes/Increment.java
@@ -1,20 +1,20 @@
-public class increment{
-public static  void main(string[]args){
-int i=0;
-system.out.println(i++);
-i++;
-system.out.println(i++) 
-system.out.println(i);
-system.out.println(i++);
+public class Increment {
+    public static void main(String[] args) {
+        int i=0;
+        System.out.println(i++); //0
+        i++;
+        System.out.println(i++);  //1
+        System.out.println(i);    //3
+        System.out.println(i++);  //3
+        
+for(int k=0; k<5; k++) {
+            System.out.println("for loop  "+k);}
 
-for(int k=0;k<5;k++){
-system.out.println("for loop"+k);}
-int j=10;
-while (j>10){
-system.out.println("while loop"+j);
-j--;
+        int j=10;
+        while(j>0) {
+            System.out.println("while loop  "+j);
+            j--;
+        
+    }
+    }
 }
-}
-}
-
-   
