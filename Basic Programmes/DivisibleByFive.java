941af80a58365a265953fa14173218f428f3fd0d
SAMARTHAGRA <samarthagrawal766@gmail.com>
2026-02-04 22:19:00 +0530

    assignment4

--- Basic Programmes/DivisibleByFive.java
+++ Basic Programmes/DivisibleByFive.java
@@ -4,11 +4,11 @@ public class DivisibleByFive {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // Input
+
         System.out.print("Enter a number: ");
         int number = sc.nextInt();
 
-        // Check divisibility by 5
+
         if (number % 5 == 0) {
             System.out.println("Is the number " + number + " divisible by 5? Yes");
         } else {
